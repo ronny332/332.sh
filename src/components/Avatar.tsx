6de023bd64459b731bdbd391bc3fc0be9b358e82
ronny332 <ron@332.sh>
2022-06-08 18:46:45 +0200

    Avatar fading.

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,13 +1,20 @@
 import avatar from '../assets/jpg/avatar.jpg';
 import React from 'react';
 import styles from './css/Avatar.module.css';
+import { Link, useLocation } from 'react-router-dom';
 
-const Avatar: React.FC = () => (
-  <div className={ styles.avatar }>
-    <div>
-      <img src={ avatar } alt='' />
+const Avatar: React.FC = () => {
+  const link = <img src={ avatar } alt='' />;
+  const location = useLocation().pathname;
+  const to = '/~ronny';
+
+  return (
+    <div className={ styles.avatar }>
+      <div>
+        { location !== to ? <Link to={ to }>{link}</Link> : link }
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export { Avatar };
