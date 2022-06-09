859c2849163359c761ccfd1c8f979bcbee707f18
ronny332 <ron@332.sh>
2022-06-09 22:26:38 +0200

    Construction color fading.

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -2,6 +2,7 @@ import avatar from '../assets/jpg/avatar.jpg';
 import { BrowserView } from 'react-device-detect';
 import React from 'react';
 import styles from './css/Avatar.module.css';
+import stylesConstruction from './css/Construction.module.css';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 const Avatar: React.FC = () => {
@@ -9,13 +10,25 @@ const Avatar: React.FC = () => {
   const navigate = useNavigate();
 
   const path = '/~ronny';
+  const onPath = location === path;
 
   const onClick = async (event: Event): Promise<void> => {
     event.preventDefault();
     navigate(path);
   };
 
-  const anchorAttr = location !== path ? { href: path, onClick } : null;
+  setTimeout(() => {
+    const cl = stylesConstruction.construction;
+    const el = document.getElementsByClassName(cl)[0];
+
+    if (onPath) {
+      el.classList.add(stylesConstruction.ronny);
+    } else {
+      el.classList.remove(stylesConstruction.ronny);
+    }
+  }, 250);
+
+  const anchorAttr = !onPath ? { href: path, onClick } : null;
   const imageAttr = { src: avatar, className: anchorAttr?.href ? '' : styles.disabled, alt: path };
 
   const image = React.createElement('img', imageAttr);
